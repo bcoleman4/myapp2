2c7e8900a69dbc1dc738fef3d67c9a9bfdfa4d41
bcoleman4 <bcoleman4@hotmail.co.uk>
2014-11-24 21:27:08 +0000

    Second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'welcome/new'
+  get    'signup'  => 'users#new'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
